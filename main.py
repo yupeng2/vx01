61067d4ff3c8950cb11615917e5f855b8000339a
yupeng2 <105643065+yupeng2@users.noreply.github.com>
2022-10-13 20:53:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from zhdate import ZhDate
 import sys
 import os
  
- 
+ def get_color():
+    # 获取随机颜色
+    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
+    color_list = get_colors(100)
+    return random.choice(color_list)
+   
 def get_access_token():
     # appId
     app_id = config["app_id"]
